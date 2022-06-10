fdc2ae46cdf16b28a93a93822139583bd1fbb55c
wooseop <crazyz2000@naver.com>
2022-06-10 14:40:34 +0900

    [server][user] edit function signup and delete
    @ user.js
    - 회원가입 시 characters collection에 empty document 추가
    - 회원탈퇴 시 characters collection에서 document 삭제
    - 회원탈퇴 router method 변경 (get -> delete)

--- server/router/user.js
+++ server/router/user.js
@@ -28,15 +28,23 @@ router.get('/logout', loginChecker.isLogin, (req, res) => {
 router.post('/signup', (req, res) => {
     let userInfo = {id: req.body.id, pw: bcrypt.hashSync(req.body.pw, 10)};
 
+    // 중복 체크를 위해 id 검색
     db.client.collection(process.env.COLLECTION_USER).findOne({id: userInfo.id}, (err, findRes) => {
         if (err) throw err;
 
         if (!findRes) {
+            // user 추가
             db.client.collection(process.env.COLLECTION_USER).insertOne(userInfo, (err, insertRes) => {
                 if (err) throw err;
 
-                console.log("[USER][SIGNUP] | id : " + userInfo.id);
-                res.send("회원가입 성공");
+                // characters collection에 document 추가
+                let emptyCharacter = {userId: userInfo.id, list: []};
+                db.client.collection(process.env.COLLECTION_CHARACTER).insertOne(emptyCharacter, (err, insertCharRes) => {
+                    if (err) throw err;
+
+                    console.log("[USER][SIGNUP] | id : " + userInfo.id);
+                    res.send("회원가입 성공");
+                });
             });
         } else {
             res.send("회원가입 실패(이미 존재하는 id)");
@@ -44,12 +52,18 @@ router.post('/signup', (req, res) => {
     });
 });
 
-router.get('/delete', loginChecker.isLogin, (req, res) => {
+router.delete('/', loginChecker.isLogin, (req, res) => {
+    // user 삭제
     db.client.collection(process.env.COLLECTION_USER).deleteOne({id: req.user.id}, (err, delRes) => {
         if (err) throw err;
 
-        console.log("[USER][DELETE] | id : " + req.user.id);
-        res.send("회원탈퇴완료");
+        // characters collection에서 document 삭제
+        db.client.collection(process.env.COLLECTION_CHARACTER).deleteOne({userId: req.user.id}, (err, delCharRes) => {
+            if (err) throw err;
+
+            console.log("[USER][DELETE] | id : " + req.user.id);
+            res.send("회원탈퇴완료");
+        });
     });
 });
 
