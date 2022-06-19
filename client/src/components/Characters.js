039b8e4b02e545e9446ea3b21f855e7e581ea3c4
wooseop <crazyz2000@naver.com>
2022-06-19 14:56:36 +0900

    [client][Characters] implement component
    @ Characters.js
    - 캐릭터 수정 및 삭제 기능 구현

--- client/src/components/Characters.js
+++ client/src/components/Characters.js
@@ -24,13 +24,51 @@ function addCharacter(cls, level, name) {
         window.location.replace("/");
     })
     .catch((err) => {
-        if (err) console.log(err);
+        console.log(err);
+    });
+}
+
+function editCharacter(index, cls, level, name) {
+    axios.put(process.env.REACT_APP_SERVER + "/characters/edit",
+    {
+        index: index,
+        newClass: cls,
+        newLevel: level,
+        newName: name
+    }, {
+        withCredentials: true
+    })
+    .then((res) => {
+        window.location.replace("/");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+}
+
+function deleteCharacter(index) {
+    axios.delete(process.env.REACT_APP_SERVER + "/characters",
+    {
+        data: {
+            index: index
+        },
+        withCredentials: true
+    })
+    .then((res) => {
+        window.location.replace("/");
+    })
+    .catch((err) => {
+        console.log(err);
     });
 }
 
 function Characters() {
     const [characterList, setCharacterList] = useState([]);
-    const [addShow, setAddShow] = useState(false);
+    const [addModalShow, setAddModalShow] = useState(false);
+    const [editModalShow, setEditModalShow] = useState(false);
+    const [editShow, setEditShow] = useState(false);
+    const [deleteShow, setDeleteShow] = useState(false);
+    const [editIndex, setEditIndex] = useState(-1);
     const [inputClass, setInputClass] = useState(null);
     const [inputLevel, setInputLevel] = useState(0);
     const [inputName, setInputName] = useState(null);
@@ -56,20 +94,54 @@ function Characters() {
             <button className="m-1 w-100 btn btn-lg btn-primary">전체 현황 보기</button>
             {
                 characterList.map((character, i) => {
-                    return (
-                        <button key={i} className="m-1 w-100 btn btn-lg btn-secondary">{`${character.name} (${character.level})`}</button>
-                    )
+                    if (deleteShow) {
+                        return (
+                            <div key={i} className="w-100 text-center">
+                                <button className="m-1 w-75 btn btn-lg btn-secondary">{`${character.name} (${character.level})`}</button>
+                                <button className="m-1 btn btn-lg btn-danger" onClick={() => {deleteCharacter(i)}}>삭제</button>
+                            </div>
+                        )
+                    } else if (editShow) {
+                        return (
+                            <div key={i} className="w-100 text-center">
+                                <button className="m-1 w-75 btn btn-lg btn-secondary">{`${character.name} (${character.level})`}</button>
+                                <button className="m-1 btn btn-lg btn-primary" onClick={() => {
+                                    setEditIndex(i);
+                                    setInputClass(character.class);
+                                    setInputLevel(character.level);
+                                    setInputName(character.name);
+                                    setEditModalShow(true);
+                                }}>수정</button>
+                            </div>
+                        )
+                    }
+                    else {
+                        return (
+                            <button key={i} className="m-1 w-100 btn btn-lg btn-secondary">{`${character.name} (${character.level})`}</button>
+                        )
+                    }
                 })
             }
             <button className="m-1 w-100 btn btn-lg btn-outline-success" onClick={() => {
-                setAddShow(true); 
+                setAddModalShow(true); 
                 setInputClass(null);
                 setInputLevel(0);
                 setInputName(null);
                 setErrMessage(null);
-            }}>+ 캐릭터 추가</button>
+            }}>캐릭터 추가</button>
+            {
+                (editShow || deleteShow) === true ? <button className="m-1 w-100 btn btn-lg btn-outline-secondary" onClick={() => {
+                                                        setEditShow(false);
+                                                        setDeleteShow(false);
+                                                    }}>취소</button>
+                                                  : <div className="m-1 w-100">
+                                                        <button className="w-50 btn btn-lg btn-outline-primary" onClick={() => {setEditShow(true)}}>수정</button>
+                                                        <button className="w-50 btn btn-lg btn-outline-danger" onClick={() => {setDeleteShow(true)}}>삭제</button>
+                                                    </div>
+                                                    
+            }
 
-            <Modal show={addShow} onHide={() => {setAddShow(false);}}>
+            <Modal show={addModalShow} onHide={() => {setAddModalShow(false)}}>
                 <Modal.Header closeButton>
                     <Modal.Title className="fs-2 fw-bold">캐릭터 추가</Modal.Title>
                 </Modal.Header>
@@ -100,7 +172,7 @@ function Characters() {
                     }
                 </Modal.Body>
                 <Modal.Footer>
-                    <button className="btn btn-secondary" onClick={() => {setAddShow(false)}}>
+                    <button className="btn btn-secondary" onClick={() => {setAddModalShow(false)}}>
                         취소
                     </button>
                     <button className="btn btn-primary" onClick={() => {
@@ -108,9 +180,54 @@ function Characters() {
                             setErrMessage("입력값을 확인하세요.");
                         } else {
                             addCharacter(inputClass, inputLevel, inputName);
-                            setAddShow(false);
+                            setAddModalShow(false);
                         }
                     }}>추가</button>
+                </Modal.Footer>
+            </Modal>
+
+            <Modal show={editModalShow} onHide={() => {setEditModalShow(false)}}>
+                <Modal.Header closeButton>
+                    <Modal.Title className="fs-2 fw-bold">캐릭터 추가</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <div className="m-1" id="inputClass">
+                        <Dropdown style={{display: "inline"}}>
+                            <Dropdown.Toggle variant="secondary">직업 선택</Dropdown.Toggle>
+                            <Dropdown.Menu>
+                                {
+                                    CLASS.map((cls, i) => {
+                                        return (
+                                            <Dropdown.Item key={i} onClick={() => {setInputClass(cls)}}>{cls}</Dropdown.Item>
+                                        )
+                                    })
+                                }
+                            </Dropdown.Menu>
+                        </Dropdown>
+                        <span className="ms-3 fw-bold">{inputClass}</span>
+                    </div>
+                    <div className="m-1" id="inputLevel">
+                        <input type="number" placeholder="아이템 레벨" min="0" value={inputLevel} onChange={(event) => {setInputLevel(event.target.value)}}></input>
+                    </div>
+                    <div className="m-1" id="inputName">
+                        <input type="text" placeholder="캐릭터명" value={inputName} onChange={(event) => {setInputName(event.target.value)}}></input>
+                    </div>
+                    {
+                        errMessage == null ? null : <div className="m-1" style={{color: "#FF0000"}}>{errMessage}</div>
+                    }
+                </Modal.Body>
+                <Modal.Footer>
+                    <button className="btn btn-secondary" onClick={() => {setEditModalShow(false)}}>
+                        취소
+                    </button>
+                    <button className="btn btn-primary" onClick={() => {
+                        if (inputClass == null || inputLevel === 0 || inputName == null) {
+                            setErrMessage("입력값을 확인하세요.");
+                        } else {
+                            editCharacter(editIndex, inputClass, inputLevel, inputName);
+                            setEditModalShow(false);
+                        }
+                    }}>수정</button>
                 </Modal.Footer>
             </Modal>
         </div>
