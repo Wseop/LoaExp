21fbddcac671108342bc06b12fdd284b079d819a
wooseop <crazyz2000@naver.com>
2022-06-26 21:44:06 +0900

    [server][accessory] implement function - add by html file
    @ 모듈 추가
    - cheerio
    - multer
    
    @ accessory.js
    - 클라이언트로부터 html파일을 전송 받아 여러개의 데이터를 한 번에 추가하는 기능 구현
    - html 확장자만 받기 위해 multer fileFilter 활용
    - cheerio로 html파일을 parsing

--- server/router/accessory.js
+++ server/router/accessory.js
@@ -5,6 +5,32 @@ const documentSetter = require('../utils/documentSetter.js');
 const loginChecker = require('../loginChecker.js');
 const db = require('../db.js');
 const ObjectId = require('mongodb').ObjectId;
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+const cheerio = require('cheerio');
+const { send } = require('process');
+
+const storage = multer.diskStorage({
+    destination: (req, file, callback) => {
+        callback(null, './public/temp')
+    },
+    filename: (req, file, callback) => {
+        callback(null, file.originalname);
+    }
+});
+const upload = multer({
+    storage: storage,
+    fileFilter: (req, file, callback) => {
+        let ext = path.extname(file.originalname);
+        if (ext !== '.html') {
+            callback(null, false);
+        } else {
+            callback(null, true);
+        }
+    }
+});
+const uploadSingleHtml = upload.single('html');
 
 // 전체 조회
 router.get('/', loginChecker.isLogin, (req, res) => {
@@ -39,6 +65,43 @@ router.post('/', loginChecker.isLogin, async (req, res) => {
         res.send(newDocument);
 
         console.log("[ACC][CREATE]");
+    });
+});
+
+// 데이터 추가 - html file
+router.post('/html', loginChecker.isLogin, async (req, res) => {
+    uploadSingleHtml(req, res, async (err) => {
+        if (req.file == null) {
+            res.status(400).send(false);
+        } else {
+            // 전송받은 파일 로드
+            let $ = cheerio.load(fs.readFileSync('./public/temp/' + req.file.filename));
+            // 우선 response 후 처리
+            res.status(200).send(true);
+            // 모든 열을 순회하며 데이터 추가 진행
+            await Promise.all($('body > div > table > tbody').children().map(async (i, element) => {
+                if (i !== 0) {
+                    let body = {};
+                    
+                    body.grade = $(element).children('td')[0].children[0].data
+                    body.part = $(element).children('td')[1].children[0].data
+                    body.quality = $(element).children('td')[2].children[0].data
+                    body.ability1 = $(element).children('td')[3].children[0].data
+                    body.ability2 = $(element).children('td')[4].children[0] == null ? null : $(element).children('td')[4].children[0].data;
+                    body.engrave1 = $(element).children('td')[5].children[0].data
+                    body.engrave2 = $(element).children('td')[6].children[0].data
+                    body.from = $(element).children('td')[7].children[0].data
+
+                    let newDocument = await documentSetter.setAccessory(req.user.id, body);
+                    await db.client.collection(process.env.COLLECTION_ACCESSORIES).insertOne(newDocument);
+                }
+            }));
+           
+            // 완료되면 파일 삭제
+            fs.unlinkSync('./public/temp/' + req.file.filename);
+
+            console.log("[ACC][CREATE][by html]");
+        }
     });
 });
 
